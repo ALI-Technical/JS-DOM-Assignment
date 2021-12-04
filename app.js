8a24e0a1bdb5d780b394e5b7f29da66c571b87e1
Decent-ALI <92889077+Decent-ALI@users.noreply.github.com>
2021-12-04 19:32:17 +0500

    DOM Assignment

---
+++ app.js
@@ -0,0 +1,79 @@
+/* PART 1 */
+
+/* Q NO.1 & 2 */
+/*
+var mainContent = document.getElementById("main-content");
+console.log(mainContent.children);*/
+
+
+/* Q NO.3 */
+
+/*
+var render = document.getElementsByClassName("render");
+var content1 = render[0].innerHTML;
+var content2 = render[1].innerHTML;
+var content3 = render[2].innerHTML;
+var content4 = render[3].innerHTML;
+var content5 = render[4].innerHTML;
+console.log(render);
+console.log(content1);
+console.log(content2);
+console.log(content3);
+console.log(content4);
+console.log(content5);
+*/
+
+/* Q NO.4 & 5 */
+/*
+var firstName = document.getElementById("first-name");
+var lastName = document.getElementById("last-name");
+var email = document.getElementById("email");
+
+firstName.value="Value From JS";
+lastName.value="Value From JS";
+email.value="Value From JS";
+*/
+
+
+/* PART 2 */
+
+/* Q NO.1 */
+
+/*
+var form_content = document.getElementById("form-content");
+var formNode = form_content.nodeType;
+console.log(formNode);
+*/
+
+
+/* Q NO.2 & 3 */
+/*
+var lastName = document.getElementById("last-name");
+console.log(lastName.nodeType);
+var lastNameChild = lastName.childNodes[0] = document.createElement("h1");
+console.log(lastNameChild);
+*/
+
+
+/* Q NO.4 */
+/*
+var mainContent = document.getElementById("main-content");
+console.log(mainContent.firstChild);
+console.log(mainContent.lastChild);
+*/
+
+
+/* Q NO.5 */
+/*
+var lastName = document.getElementById("last-name");
+console.log(lastName.nextElementSibling);
+console.log(lastName.previousElementSibling);
+*/
+
+
+/* Q NO.6 */
+/*
+var email = document.getElementById("email");
+console.log(email.parentNode);
+console.log(email.nodeType);
+*/
